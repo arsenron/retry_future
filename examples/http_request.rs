fc4774f054aec0cb4aaf116547c879da48c56600
arsenron <arsenron@gmail.com>
2022-10-11 19:10:36 +0300

    Init

---
+++ examples/http_request.rs
@@ -0,0 +1,27 @@
+use reqwest::StatusCode;
+use async_retry::*;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let resp = FutureRetry::new(
+        || async {
+            let resp = reqwest::get("http://localhost:8084").await?;
+            if resp.status() == StatusCode::BAD_REQUEST {
+                Err(RetryPolicy::Fail("Cannot recover from bad request"))
+            } else if resp.status() == StatusCode::INTERNAL_SERVER_ERROR {
+                Err(RetryPolicy::Repeat)
+            } else {
+                Ok(resp)
+            }
+        },
+        LinearRetryStrategy::default()
+            .duration_between_repeats(Duration::from_secs(5))
+            .attempts(1),
+    )
+    .await?;
+
+    eprintln!("resp = {:#?}", resp);
+
+    Ok(())
+}
